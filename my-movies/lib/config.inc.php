e949987eaf5ecb75ad6d50657901750b2882b8f8
Ivan Groenewold <9805809+igroene@users.noreply.github.com>
2021-09-21 11:38:14 -0300

    fix imdb password (#4)

--- my-movies/lib/config.inc.php
+++ my-movies/lib/config.inc.php
@@ -5,7 +5,7 @@ define('BASE_URI', '/my-movies/');
 define('MYSQL_HOST', 'localhost');
 define('MYSQL_USER', 'imdb');
 define('MYSQL_DATABASE', 'imdb');
-define('MYSQL_PASSWORD', 'imdb');
+define('MYSQL_PASSWORD', 'imDb1234#');
 
 define('DEBUG_MODE', false);
 
